7b1ba9d7cf532c92e6492a53aa5abcc191060a46
Omar Irfan <omar.irfan18@gmail.com>
2021-07-21 14:11:46 -0400

    pokemon component desing

--- front-end/src/Pokemon.js
+++ front-end/src/Pokemon.js
@@ -1,4 +1,27 @@
 import React from "react"
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid'
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: '#d9d9d9',
+    backgroundImage: 'linear-gradient(315deg, #d9d9d9 0%, #f6f2f2 74%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px grey',
+    color: 'DarkBlue',
+    width: '60%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+}));
 
 function Pokemon() {
   const testPokemon = {
@@ -22,18 +45,42 @@ function Pokemon() {
       "Speed": 45
     }
   }
-  return (
-    <li className="PokemonCard">
-      <h2>ID: {testPokemon.id}</h2>
-      <h2>Name: {testPokemon.name.english}</h2>
-      <h2>Type: {testPokemon.type}</h2>
-      <h2>HP: {testPokemon.base.HP}</h2>
-      <h2>ATTACK: {testPokemon.base.Attack}</h2>
-      <h2>DEFENSE: {testPokemon.base.Defense}</h2>
-      <h2>SPEED: {testPokemon.base.Speed}</h2>
+  const testPokemonImg = "https://i.imgur.com/mgQBTVE.png"
 
-    </li>
-  )
+  const testPokemonSprite = "https://i.imgur.com/yd6Y2Qw.png"
+  const classes = useStyles();
+
+  return (
+
+
+      <Accordion className={classes.root}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Grid className={classes.heading} container direction="row" alignItems="center"
+          justifyContent="space-between"
+          >
+          <img src={testPokemonSprite} />
+          <Typography>{testPokemon.name.english}</Typography>
+          <Typography>Pokédex ID: {testPokemon.id}
+          </Typography>
+          </Grid>
+        </AccordionSummary>
+        <AccordionDetails>
+          <img src={testPokemonImg}/>
+        <Grid className={classes.heading} container direction="column" alignItems="flex-end">
+          <Typography>Types: {testPokemon.type.toString().split(",").join(", ")}</Typography>
+          <Typography>HP: {testPokemon.base.HP}</Typography>
+          <Typography>Attack: {testPokemon.base.Attack}</Typography>
+          <Typography>Defense: {testPokemon.base.Defense}</Typography>
+          <Typography>Speed: {testPokemon.base.Speed}</Typography>
+        </Grid>
+        </AccordionDetails>
+      </Accordion>
+
+  );
 }
 
 export default Pokemon;
